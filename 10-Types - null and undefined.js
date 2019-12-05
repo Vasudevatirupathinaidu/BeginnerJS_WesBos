b7b2c4322e0a24c87282dec25f1e26f17664d9f2
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2019-12-05 14:28:47 +0530

    Types - null and undefined

---
+++ 10-Types - null and undefined.js
@@ -0,0 +1,26 @@
+// Undefined
+
+let dog;
+console.log(dog);
+dog = 'hugo';
+
+// Null
+
+const somethingNull = null;
+console.log(somethingNull);
+
+const cher = {
+    first: 'cher',
+};
+
+console.log(cher.last);
+
+const teller = {
+    first: 'Reymond',
+    last: 'Teller'
+}
+
+teller.first = 'Teller';
+teller.last = null;
+
+console.log(teller);
