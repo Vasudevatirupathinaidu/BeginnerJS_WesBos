0cda2e055872aeba6821b6ff45dd34421f71602e
Deva <33352031+Vasudevatirupathinaidu@users.noreply.github.com>
2019-12-07 10:56:16 +0530

    Functions - Custom

---
+++ 13-Functions - Custom.js
@@ -0,0 +1,13 @@
+// function definition
+function calculateBill() {
+  // this is the function body
+  console.log('Running Calculate Bill!!');
+  const total = 100 * 1.13;
+  return total;
+}
+
+// function call or run or invoke
+// const myTotal = calculateBill();
+// console.log(`Your total is $${myTotal}`);
+// console.log(calculateBill());
+console.log(`Your total is $${calculateBill()}`);
